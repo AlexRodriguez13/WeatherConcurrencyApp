d7fa870de9aaca1e82534cbf7a246d7f9cd0005a
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-28 09:28:12 -0600

    IWeather

---
+++ WeatherConcurrentApp.Domain/Interfaces/IWeather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using WeatherConcurrentApp.Domain.Entities;
+
+namespace WeatherConcurrentApp.Domain.Interfaces
+{
+   public interface IWeather: IModel<OpenWeather>
+    {
+        List<OpenWeather> findWByCity(Expression<Func<OpenWeather, bool>> where);
+    }
+}

d19940139c379c8301fdbffdc5b92a53f6683577
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-28 10:08:20 -0600

    BInary Reader Extension

---
+++ WeatherConcurrencyApp.Infrastructure/Almacenamiento/BinaryReaderExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherConcurrencyApp.Infrastructure.Almacenamiento
+{
+    public class BinaryReaderExtension
+    {
+
+    }
+}
